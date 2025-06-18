f1fdf29a4fb224f7f03e7b394f51ef76cded69b0
anish <anishgautam2024@gmail.com>
2025-06-18 22:05:47 +0530

    organize CSS with Tailwind v4 functional utilities and global theme variables for layout padding

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,8 @@ import NavBar from './components/NavBar'
 
 const App = () => {
   return (
-    <div className='w-full h-dvh'>
-      <h1 className="text-3xl font-bold text-center underline text-foreground py-3">
+    <div className='w-full h-dvh page-padding'>
+      <h1 className="text-3xl font-bold text-center underline text-foreground py-3 ">
         MakeUp Store
       </h1>
       <NavBar />
